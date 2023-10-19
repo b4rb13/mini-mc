435d53605cd60f8a8cf003713b07b7e269e9352a
andrey.arakelyan <andrey.arakelyan@sentium-consulting.com>
2023-10-20 01:17:35 +0400

    fix: add fix for radix/react-dialog

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const blackOpsOne = Black_Ops_One({ weight: '400', subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Mini-MC - File Converter',
   description: `Media files converter`,
-  creator: 'Andrey Arakelyan, Derenik Khachatryan',
+  creator: 'Derenik Khachatryan, Andrey Arakelyan',
   keywords: 'image converter, video converter, audio converter, unlimited image converter, unlimited video converter',
 };
 
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <GA GA_MEASUREMENT_ID="mer GA ID-n" />
       <meta name="google-site-verification" content="heto generate kanenq" />
-      <body className={blackOpsOne.className}>
+      <body className={blackOpsOne.className} style={{ minWidth: '390px' }}>
         <Navbar />
         <Toaster />
         <div className="pt-32 min-h-screen lg:pt-36 2xl:pt-44 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">

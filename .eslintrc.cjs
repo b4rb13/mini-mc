b0ca841512f9035feade93a1de33f19460ef6bc3
andrey.arakelyan <andrey.arakelyan@sentium-consulting.com>
2023-10-20 18:02:53 +0400

    feat: eslint plus prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,18 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "off",
+    "prettier/prettier": ["error"]
   },
 }

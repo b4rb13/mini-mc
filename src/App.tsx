cc7037230ed12169418c3d5e7f4e48a50b6b5a9f
andrey.arakelyan <andrey.arakelyan@sentium-consulting.com>
2023-10-22 02:02:34 +0400

    feat: add buy me a coffee button and link

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Dropzone from '@/components/dropzone.tsx';
 import { Toaster } from '@/components/ui/toaster.tsx';
+import { Button } from '@/components/ui/button.tsx';
+import { DiCoffeescript } from 'react-icons/di';
 
 function App() {
   return (
-    <div className="pt-32 min-h-screen lg:pt-36 2xl:pt-44 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
+    <div className="pt-8 min-h-screen lg:pt-36 2xl:pt-44 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
       <Toaster />
       <div className="space-y-16 pb-8">
         {/* Title + Desc */}
@@ -15,6 +17,19 @@ function App() {
         </div>
         {/* Upload Box */}
         <Dropzone />
+      </div>
+      <div className={'flex justify-center items-center'}>
+        <Button
+          onClick={() => window.open('https://www.buymeacoffee.com/minimc', '_blank')}
+          variant="default"
+          className="rounded-full w-fit bg-gradient-1 gap-2 items-center md:flex"
+          size="lg"
+        >
+          <span>Buy me a Coffee</span>
+          <span className="text-4xl">
+            <DiCoffeescript />
+          </span>
+        </Button>
       </div>
     </div>
   );

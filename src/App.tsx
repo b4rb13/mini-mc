f64e6b89e7cc3e0ad5cf0e5d01e9b2cef215a9e4
andrey.arakelyan <andrey.arakelyan@sentium-consulting.com>
2023-10-22 00:48:28 +0400

    feat: updated ffmpeg version and base url

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster } from '@/components/ui/toaster.tsx';
 function App() {
   return (
     <div className="pt-32 min-h-screen lg:pt-36 2xl:pt-44 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
+      <Toaster />
       <div className="space-y-16 pb-8">
         {/* Title + Desc */}
         <div className="space-y-6">
@@ -15,7 +16,6 @@ function App() {
         {/* Upload Box */}
         <Dropzone />
       </div>
-      <Toaster />
     </div>
   );
 }
--- src/utils/load-ffmpeg.ts
+++ src/utils/load-ffmpeg.ts
@@ -4,7 +4,7 @@ import { toBlobURL } from '@ffmpeg/util';
 
 export default async function loadFfmpeg(): Promise<FFmpeg> {
   const ffmpeg = new FFmpeg();
-  const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.2/dist/umd';
+  const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.4/dist/esm';
   await ffmpeg.load({
     coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
     wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
